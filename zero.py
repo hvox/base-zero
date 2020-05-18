0d1cd1c4d9ab8eb5d50904273276f5f49f4a09a2
Uladzislau Khamkou <hamk.vo@gmail.com>
2020-05-18 16:25:21 +0300

    fix: move alphabet from source to alphabet.bin

--- zero.py
+++ zero.py
@@ -1,4 +1,4 @@
-alphabet = ' ̴̵̶̷̸̡̢̧̨̛̖̗̘̙̜̝̞̟̠̣̤̥̦̩̪̫̬̭̮̯̰̱̲̳̹̺̻̼͇͈͉͍͎̀́̂̃̄̅̆̇̈̉̊̋̌̍̎̏̐̑̒̓̔̽̾̿̀́͂̓̈́͆͊͋͌̕̚ͅ͏͓͔͕͖͙͚͐͑͒͗͛ͣͤͥͦͧͨͩͪͫͬͭͮͯ҃҄҅҆҇͘͜͟͢͝͞͠͡҈҉߲߫߬߭߮߯߰߱߳ఀ᩿᪵᪶᪷᪸᪹᪺᪽፝፞፟᪰᪱᪲᪳᪴᪻᪼᪾⃒⃓⃘⃙⃚᷐᷎᭬᷂᷊᷏᷽᷿᭫᭭᭮᭯᭰᭱᭲᭳᷀᷁᷃᷄᷅᷆᷇᷈᷉᷋᷌᷑᷒ᷓᷔᷕᷖᷗᷘᷙᷚᷛᷜᷝᷞᷟᷠᷡᷢᷣᷤᷥᷦᷧᷨᷩᷪᷫᷬᷭᷮᷯᷰᷱᷲᷳᷴ᷵᷾⃐⃑⃔⃕⃖⃗⃛⃜᷼᷍⃝⃞⃟⃠⃡⃢⃣⃤⃥⃦⃪⃫⃨⃬⃭⃮⃯⃧⃩⃰⳯⳰⳱ⷠⷡⷢⷣⷤ '.strip()
+alphabet = open("alphabet.bin").read()
 rev = {c: i for i, c in enumerate(alphabet)}
 encode = lambda data: "".join((alphabet[b] for b in data))
 decode = lambda data: (rev[c] for c in data)

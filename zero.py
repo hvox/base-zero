3938dc569bf143291af96f4ec0beaff0f199b90c
Uladzislau Khamkou <kham@tuta.io>
2022-02-20 22:20:03 +0300

    feat: implement self-extracting encoding

--- zero.py
+++ zero.py
@@ -2,3 +2,4 @@ alphabet = open("alphabet.bin").read()
 rev = {c: i for i, c in enumerate(alphabet)}
 encode = lambda data: "".join((alphabet[b] for b in data))
 decode = lambda data: (rev[c] for c in data)
+encode_code = lambda s: f'eval(bytes(map("{alphabet}".index, "{encode(s)}")))'

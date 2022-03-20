34495dc7a80cf37893c6c82c690833f26983ef89
Uladzislau Khamkou <kham@tuta.io>
2022-03-20 17:10:19 +0300

    feat: command line interface

--- zero.py
+++ zero.py
@@ -3,3 +3,9 @@ rev = {c: i for i, c in enumerate(alphabet)}
 encode = lambda data: "".join((alphabet[b] for b in data))
 decode = lambda data: (rev[c] for c in data)
 encode_code = lambda s: f'eval(bytes(map("{alphabet}".index, "{encode(s)}")))'
+
+if __name__ == "__main__":
+    import sys
+    from pathlib import Path
+
+    Path(sys.argv[2]).write_text(encode_code(Path(sys.argv[1]).read_bytes()))
